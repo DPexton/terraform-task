6a1f7f39d9544f7607f7345e8bb03934945ebc93
david <davidpj@hotmail.co.uk>
2021-06-22 15:55:46 +0000

    first attempt, feature blocks error

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = "= 2.14.0"
+  }
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_Bs1"
+  }
+
+  service_principal {
+    client_id     = "00000000-0000-0000-0000-000000000000"
+    client_secret = "00000000000000000000000000000000"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "example" {
+  name                  = "internal"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
+  vm_size               = "Standard_B1s"
+  node_count            = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
